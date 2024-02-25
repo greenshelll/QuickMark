550fd2256d986573e111350b022889bf04cf3b93
Eugene Dave Tumagan <eugenetumagan02@gmail.com>
2024-02-25 22:07:19 +0800

    saved local copy; updated omr

---
+++ src/helloworld/capture.py
@@ -0,0 +1,68 @@
+try:
+    import helloworld.boxer as boxer
+    import helloworld.omr as omr
+except Exception:
+    import boxer
+    import omr
+import math
+import cv2
+
+class TestType:
+    class MultipleChoice:
+        def __init__(self, items):
+            self.name = 'MULTIPLE CHOICE'
+            self.num_items = items
+            self.circles_per_num = 4
+    class TrueOrFalse:
+        def __init__(self, items):
+            self.name ='TRUE OR FALSE'
+            self.num_items = items
+            self.circles_per_num = 2
+
+def get_scores(target_img, ground_mc, ground_tf, target_ispath = True, return_result_image = False):
+    
+    # Gets boxes in image
+    rectangles, image_result, cropped_result, orig_image = boxer.get_boxes(target_img, 1,target_ispath)
+
+    #omr.show_img(image_result)
+    # gets first cropped image (largest box detected)
+    cropped_result = cropped_result[0]
+
+    # gets ALL bubble choices
+    circles, circle_image_result, orig_image = omr.get_choices(cropped_result, False)
+    #omr.show_img(circle_image_result)
+
+    # detects all rows
+    circles_by_row = omr.get_rows(circles)
+    #print(circles_by_row)
+
+    # get type of test
+    test_type_inference = omr.get_type_test(circles_by_row,True,False)
+    if test_type_inference == 'MULTIPLE CHOICE':
+        # set object info to mc
+        test_type_inference = ground_mc
+    if test_type_inference == 'TRUE OR FALSE':
+        # set object info to tf
+        test_type_inference = ground_tf
+
+    # check validity; ensure all bubbles are included
+    if test_type_inference.num_items*test_type_inference.circles_per_num == len(circles):
+        # if complete, get number for each group of bubble (ex. by 4 for MC, by 2 for TF)
+        result_complete = omr.get_by_num(test_type_inference.circles_per_num, circles_by_row)
+        # -------------------(IF....) for debugging purposes only; verify each num
+        if return_result_image:
+            for num in range(1,test_type_inference.num_items+1):
+                content = result_complete[num]
+                for circles in content:
+                    x, y, w, h = circles.xywh
+                    cv2.rectangle(orig_image, (x, y), (x + w, y + h), (255, 0, 0), 5)
+                omr.show_img(orig_image)
+            return orig_image
+        # -------------------(ENDIF)
+        return result_complete
+    else:
+        return None
+
+result_complete = (get_scores(r"C:\Users\USER\Downloads\samples\IMG_20240225_084909.jpg", 
+                            TestType.MultipleChoice(100), TestType.TrueOrFalse(75),target_ispath=True,return_result_image=False))
+print(result_complete.keys())
