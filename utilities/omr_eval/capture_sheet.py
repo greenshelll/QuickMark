5e89f4f314fb5bd0185b307bcb629620043532be
Eugene Dave Tumagan <eugenetumagan02@gmail.com>
2024-03-12 06:53:17 +0800

    some ocr... added file syst

--- utilities/omr_eval/capture_sheet.py
+++ utilities/omr_eval/capture_sheet.py
@@ -183,25 +183,3 @@ class CaptureSheet:
                                                     CaptureSheet_obj=self,
                                                     box_index=crop_i)
     
-if __name__ == "__main__":
-    # sample input
-    #cs = CaptureSheet(100,10,10,r"C:\Users\USER\Downloads\WIN_20240228_07_05_54_Pro.jpg",True,True)
-    cs = CaptureSheet(
-        mcq_items=100,
-        tfq_items=10,
-
-        idq_items=10,
-        img=r"C:\Users\USER\Downloads\430920436_1065062367937575_3927357814054852515_n.jpg",
-        boxes_num=1,
-        get_result_img=True,
-        show_plots=True,
-        on_android=False
-    )
-    cs.get_boxes()
-    cs.get_bubbles(True)
-    cs.get_choices()
-    cs.get_scores()
-    print(cs.bubbles[0].count)
-    print(cs.bubbles[0].final_score)
-else:
-    pass
